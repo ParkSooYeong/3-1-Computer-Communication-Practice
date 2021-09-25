42978a1c3ef6345f022acf99d97cba1fa2b229e0
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-09-26 00:13:31 +0900

    Add files via upload

---
+++ Chapter 1/3-1-1-002 Event-Driven2.js
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 1 , Number 2 */
+
+function run()
+{
+    console.log('3초 후 실행');
+}
+console.log('시작');
+setTimeout(run, 3000);
+console.log('끝');

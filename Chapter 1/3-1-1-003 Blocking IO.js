eb160bc9669b00c492534f4a1dc91a535456e7fc
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-09-27 08:02:33 +0900

    Add files via upload

---
+++ Chapter 1/3-1-1-003 Blocking IO.js
@@ -0,0 +1,11 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 1 , Number 3 */
+
+function longRunningTask()
+{
+    // 오래 걸리는 작업
+    console.log('작업 끝');
+}
+console.log('시작');
+longRunningTask();
+console.log('다음 작업');

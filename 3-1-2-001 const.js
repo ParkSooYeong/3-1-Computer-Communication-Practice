97126774b49800fa9c6b332cca6e6b25300675e7
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-09-29 03:37:45 +0900

    Add files via upload

---
+++ 3-1-2-001 const.js
@@ -0,0 +1,14 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 2 , Number 1 */
+
+if(true)
+{
+    var x = 3;
+}
+console.log(x);
+
+if(true)
+{
+    const y = 3;
+}
+console.log(y);

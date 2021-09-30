fe9a28a4a8ab2bad697e7b9cc2c5e96c7bca7df9
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-10-01 07:41:03 +0900

    Add files via upload

---
+++ Chapter 2/3-1-2-003 Template String.js
@@ -0,0 +1,14 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 2 , Number 3 */
+
+var num1 = 1;
+var num2 = 2;
+var result = 3;
+var string1 = num1 + ' 더하기 ' + num2 + '는 \'' + result + '\'';
+console.log(string1); // 1 더하기 2는 '3'
+
+const num3 = 1;
+const num4 = 2;
+const result2 = 3;
+const string2 = `${num3} 더하기 ${num4}는 '${result2}'`;
+console.log(string2); // 1 더하기 2는 '3'

d374ae4edcef8cc165cdb44570e7f002242c1958
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-10-03 00:18:36 +0900

    Add files via upload

---
+++ Chapter 2/3-1-2-005 Arrow Function1.js
@@ -0,0 +1,22 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 2 , Number 5 */
+
+function add1(x, y)
+{
+    return x + y;
+}
+
+const add2 = (x, y) => {
+    return x + y;
+};
+
+const add3 = (x, y) => x + y;
+
+const add4 = (x, y) => (x + y);
+
+function not1(x)
+{
+    return !x;
+}
+
+const not2 = x => !x;

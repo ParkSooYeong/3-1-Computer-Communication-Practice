0101367291a85330981644ef703117cee576a966
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-10-06 08:02:24 +0900

    Add files via upload

---
+++ Chapter 2/3-1-2-008 Destructuring Assignment2.js
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 2 , Number 8 */
+
+var array = ['nodejs', {}, 10, true];
+var node = array[0];
+var obj = array[i];
+var bool = array[array.length - 1];
+
+const array = ['nodejs', {}, 10, true];
+const [node, obj, bool] = array;

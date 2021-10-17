38e908d059e7b85ff3613de97fc2ee8da1f228f3
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-10-18 07:49:14 +0900

    Add files via upload

---
+++ 3-1-3-1 Hello.js
@@ -0,0 +1,13 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 3 , Number 1 */
+
+function helloWorld()
+{
+	console.log('Hello World');
+	helloNode();
+}
+function helloNode()
+{
+	console.log('Hello Node');
+}
+helloWorld();

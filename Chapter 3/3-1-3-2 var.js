884195ee83e7c282dadee022ae573a7d2a76e7b9
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-10-19 01:36:40 +0900

    Add files via upload

---
+++ Chapter 3/3-1-3-2 var.js
@@ -0,0 +1,13 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 3 , Number 2 */
+
+/* const odd = '홀수입니다.';
+const even = '짝수입니다.';
+
+module.exports = {
+    odd,
+    even,
+}; */
+
+exports.odd = '홀수입니다';
+exports.even = '짝수입니다.';

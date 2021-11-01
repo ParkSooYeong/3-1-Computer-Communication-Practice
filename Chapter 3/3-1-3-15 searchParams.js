359294941d04f5696a9b7701feaf6bb9f4d2f2fa
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-11-01 20:08:05 +0900

    Add files via upload

---
+++ Chapter 3/3-1-3-15 searchParams.js
@@ -0,0 +1,26 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 3 , Number 15 */
+
+const { URL } = require('url');
+
+const myURL = new URL('https://github.com/ParkSooYeong');
+console.log('searchParams : ', myURL.searchParams);
+console.log('searchParams.getAll() : ', myURL.searchParams.getAll('category'));
+console.log('searchParams.get() : ', myURL.searchParams.get('limit'));
+console.log('searchParams.has() : ', myURL.searchParams.has('page'));
+
+console.log('searchParams.keys() : ', myURL.searchParams.keys());
+console.log('searchParams.values() : ', myURL.searchParams.values());
+
+myURL.searchParams.append('filter', 'es3');
+myURL.searchParams.append('filter', 'es5');
+console.log(myURL.searchParams.getAll('filter'));
+
+myURL.searchParams.set('filter', 'es6');
+console.log(myURL.searchParams.getAll('filter'));
+
+myURL.searchParams.delete('filter');
+console.log(myURL.searchParams.getAll('filter'));
+
+console.log('searchParams.toString() : ', myURL.searchParams.toString());
+myURL.search = myURL.searchParams.toString();

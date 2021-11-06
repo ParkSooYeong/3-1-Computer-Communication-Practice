0548218e5058355d16aed5cbd87fafbc0b055b56
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-11-06 11:48:32 +0900

    Add files via upload

---
+++ Chapter 3/3-1-3-18 pbkdf2.js
@@ -0,0 +1,12 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 3 , Number 18 */
+
+const crypto = require('crypto');
+
+crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+    console.log('salt : ', salt);
+    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+        console.log('password : ', key.toString('base64'));
+    });
+});

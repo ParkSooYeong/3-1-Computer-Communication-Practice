57cd07ad64f9f328410d651d3873b3f6c970a2d8
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-11-10 02:30:35 +0900

    Add files via upload

---
+++ Chapter 3/3-1-3-22 async.js
@@ -0,0 +1,28 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 3 , Number 22 */
+
+const fs = require('fs');
+
+console.log('시작');
+fs.readFile('./3-1-3-22 readme2.txt', (err, data) => {
+    if(err)
+    {
+        throw err;
+    }
+    console.log('1번', data.toString());
+});
+fs.readFile('./3-1-3-22 readme2.txt', (err, data) => {
+    if(err)
+    {
+        throw err;
+    }
+    console.log('2번', data.toString());
+});
+fs.readFile('./3-1-3-22 readme2.txt', (err, data) => {
+    if(err)
+    {
+        throw err;
+    }
+    console.log('3번', data.toString());
+});
+console.log('끝');

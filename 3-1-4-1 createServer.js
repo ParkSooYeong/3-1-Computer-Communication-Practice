e8beeebe0e1f4f14e8cdfa2546129c1a00b9c8db
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-11-27 00:25:06 +0900

    Add files via upload

---
+++ 3-1-4-1 createServer.js
@@ -0,0 +1,8 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 1 , Chapter 4 , Number 1 */
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    // 여기에 어떻게 응답할지 적어줍니다.
+});
